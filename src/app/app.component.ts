4219cd98569e2745664a033da71849ce6d178103
RajatGarg <rg6693@gmail.com>
2018-03-05 14:19:04 +0530

    Add service for product data
    Along with service, it will add yarn lock and updated .gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ProductService } from './product-list/product.service';
 
 
 // A decorator (ie. a function taking one object as an argument, and this object has 3 properties)
 @Component({
   selector: 'apm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
 })
 export class AppComponent {
   pageTitle: string = 'ACME Product Management';
