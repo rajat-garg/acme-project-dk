a483018e6f342f739fa83ff407af9be1a3beac1d
RajatGarg <rg6693@gmail.com>
2018-03-05 10:07:38 +0530

    Add strong typing for product array
    Strong typing support by creating an interface

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
   selector: 'apm-product-list',
@@ -11,7 +12,7 @@ export class ProductListComponent implements OnInit {
   imageMargin: number = 2;
   showImage: boolean = false;
   listFilter: string = 'cart';
-  products: any[] = [
+  products: IProduct[] = [
     {
       "productId": 1,
       "productName": "Leaf Rake",
