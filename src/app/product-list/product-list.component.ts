1a48eaf952815021ea578871917d8031f9ef7bac
RajatGarg <rg6693@gmail.com>
2018-03-05 11:24:46 +0530

    Add search filters

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,7 +11,17 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+  _listFilter: string;
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+  filteredProducts: IProduct[];
   products: IProduct[] = [
     {
       "productId": 1,
@@ -54,12 +64,22 @@ export class ProductListComponent implements OnInit {
       "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
     }
   ];
-  constructor() { }
+
+  constructor() {
+    this.filteredProducts = this.products;
+    this._listFilter = 'cart';
+  }
 
   ngOnInit() {
   }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  //Return a new array of matched products
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1)
   }
 }
